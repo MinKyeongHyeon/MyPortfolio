8b818f6b907fe7454acb007ab880405a7aac2889
MinKyeongHyeon <28599@naver.com>
2025-10-24 21:06:27 +0900

    feat/포트폴리오용 페이지 작성

---
+++ my-app/src/pages/Home.jsx
@@ -0,0 +1,27 @@
+export default function Home() {
+  return (
+    <>
+      <h1>홈페이지</h1>
+      <div>
+        <p>여기에 내 소개</p>
+        <div>
+          <h2>민경현</h2>
+          <img src="/" alt="내 사진" />
+        </div>
+        <div>
+          <h2>여기에 스킬 소개?</h2>
+          <p>내 스킬 HTML5 ****</p>
+          <p>내 스킬 CSS3 ****</p>
+          <p>내 스킬 Javascript ****</p>
+        </div>
+        <div>
+          <h3>여기에 프로젝트 소개?</h3>
+          <p>오픈마켓?</p>
+          <p>1만시간의 법칙?</p>
+          <p>감귤마켓</p>
+          <p>두근두근 비밀일기</p>
+        </div>
+      </div>
+    </>
+  );
+}
