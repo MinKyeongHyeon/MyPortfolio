6c299f446a73e60742a892439330bc2c623cc1aa
MinKyeongHyeon <28599@naver.com>
2025-11-10 22:49:47 +0900

    feat/새로운페이지 라우팅

---
+++ my-portfolio/src/app/aboutme/page.tsx
@@ -0,0 +1,4 @@
+function About (){
+  return <h2>내 소개</h2>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis unde nemo vitae rerum quam accusantium vero nam minima, nulla ex inventore? Nisi quasi nesciunt ab architecto, nemo officiis quidem impedit?</p>
+}
--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,5 +1,16 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>나의 포트폴리오 페이지</h1>;
+  return (
+    <div>
+      {" "}
+      <h1>나의 포트폴리오 페이지</h1> 
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque tenetur
+        veritatis deserunt expedita ipsum optio eveniet omnis dolor, fuga
+        voluptas suscipit beatae sint labore possimus eligendi sapiente aliquid
+        incidunt facilis.
+      </p>
+    </div>
+  );
 }
