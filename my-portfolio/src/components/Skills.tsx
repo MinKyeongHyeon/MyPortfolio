e4671932758813a329074ebe2859f5f10eb74d01
MinKyeongHyeon <28599@naver.com>
2025-11-13 18:27:47 +0900

    feat/기술 컴포넌트 작성

---
+++ my-portfolio/src/components/Skills.tsx
@@ -0,0 +1,30 @@
+export default function Skills() {
+  return (
+    <ul>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>HTML5</span>
+      </li>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>CSS3</span>
+      </li>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>JavaScript</span>
+      </li>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>TypeScript</span>
+      </li>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>React</span>
+      </li>
+      <li>
+        <span aria-label="숙련도">9.5</span>
+        <span>Next.js</span>
+      </li>
+    </ul>
+  );
+}
