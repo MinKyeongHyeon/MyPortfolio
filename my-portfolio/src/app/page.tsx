c7625f5dc2ae3b76e3d0c0ae609de19684d7f570
MinKyeongHyeon <28599@naver.com>
2025-11-14 14:19:18 +0900

    feat/링크연결, 로렘삭제

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -1,16 +1,8 @@
-import Image from "next/image";
-
+import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      {" "}
-      <h1>나의 포트폴리오 페이지</h1> 
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque tenetur
-        veritatis deserunt expedita ipsum optio eveniet omnis dolor, fuga
-        voluptas suscipit beatae sint labore possimus eligendi sapiente aliquid
-        incidunt facilis.
-      </p>
+      <h1>나의 포트폴리오 페이지</h1> <Link href="/home">구경하기</Link>
     </div>
   );
 }
