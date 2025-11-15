4db27a87dd4ec0f91904d45981822eda320c1980
MinKyeongHyeon <28599@naver.com>
2025-11-15 22:48:07 +0900

    feat/더 좋은 느낌을 전달해 줄 수 있는 인트로 문장으로 변경

--- my-portfolio/src/components/Intro.tsx
+++ my-portfolio/src/components/Intro.tsx
@@ -1,8 +1,7 @@
 export default function Intro() {
+  const text =
+    "Hi! I’m MinKyeonghyeon, a frontend developer who loves crafting clean UIs and playful interactions. Welcome to my site!";
   return (
-    <p className="mt-5 mb-10 w-[350px] font-mono text-2xl break-all">
-      Hello world! Welcome to my web. I&apos;m Frontend Developer from South
-      Korea
-    </p>
+    <p className="mt-5 mb-10 w-[350px] font-mono text-2xl break-all">{text}</p>
   );
 }
