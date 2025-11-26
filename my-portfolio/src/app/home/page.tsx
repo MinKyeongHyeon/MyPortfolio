4c7581ec1418238b1b02f71769d0ad82b72fb207
MinKyeongHyeon <28599@naver.com>
2025-11-26 22:49:08 +0900

    feat/연락처와 바닥 아이콘 추가 및 UI정리

--- my-portfolio/src/app/home/page.tsx
+++ my-portfolio/src/app/home/page.tsx
@@ -4,6 +4,9 @@ import Skills from "@/components/Skills";
 import Runtime from "@/components/Runtime";
 import Intro from "@/components/Intro";
 import PortfolioCard from "@/components/PortfolioCard";
+import Contact from "@/components/Contact";
+import BottomIcon from "@/components/BottomIcon";
+
 import project1Img from "../../../public/images/tengerine.png";
 
 export default function About() {
@@ -25,39 +28,38 @@ export default function About() {
     },
   ];
   return (
-    <div className=" flex justify-center">
-      <main className="flex flex-col gap-3">
-        {/* 프로필 섹션 */}
-        <Profile />
-        <Intro />
+    <div className="h-screen flex justify-center">
+      <div className="flex flex-col justify-between">
+        <main className="flex flex-col gap-3">
+          {/* 프로필 섹션 */}
+          <Profile />
+          <Intro />
 
-        {/* 나의 성장 지표 */}
-        <Runtime />
+          {/* 나의 성장 지표 */}
+          <Runtime />
 
-        {/* 기술 스택 섹션 */}
-        <Card title="Skills">
-          <Skills />
-        </Card>
+          {/* 기술 스택 섹션 */}
+          <Card title="Skills">
+            <Skills />
+          </Card>
 
-        {/* 포트폴리오 섹션 */}
-        <Card title="Portfolio" backgroundColor="#485E2E">
-          <ul className="flex flex-col gap-4 my-5">
-            {project.map((project, index) => (
-              <PortfolioCard key={index} project={project} />
-            ))}
-          </ul>
-        </Card>
+          {/* 포트폴리오 섹션 */}
+          <Card title="Portfolio" backgroundColor="#485E2E">
+            <ul className="flex flex-col gap-4 my-5">
+              {project.map((project, index) => (
+                <PortfolioCard key={index} project={project} />
+              ))}
+            </ul>
+          </Card>
 
-        {/* 연락처 섹션 */}
-        <Card title="Contact" backgroundColor="#BDBDBD">
-          <p className="font-mono text-lg text-white mt-3.5">
-            Email : manemin2@gmail.com
-          </p>
-          <p className="font-mono text-lg text-white mb-3.5">
-            Phone : 010-4858-2859
-          </p>
-        </Card>
-      </main>
+          {/* 연락처 섹션 */}
+          <Card title="Contact" backgroundColor="#BDBDBD">
+            <Contact />
+          </Card>
+        </main>
+        {/* 바닥 아이콘 */}
+        <BottomIcon />
+      </div>
     </div>
   );
 }
